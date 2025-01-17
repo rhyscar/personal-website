275322bd2d44b701bf85c155117490916f7148c4
rhyscar <rhyscarroll22@gmail.com>
2025-01-17 21:46:01 +1100

    Adding files to new repo

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <>
+    <header>
+      <nav>
+        <h1>Rhys Carroll</h1>
+        <a>Hello</a>
+      </nav>
+    </header>
+    </>
+  )
+}
+
+export default App
