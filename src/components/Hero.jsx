6106fde06637f43114acc2f524437f75cbfe8a2a
rhyscar <rhyscarroll22@gmail.com>
2025-02-03 16:49:46 +1100

    Added Resume to site

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -19,7 +19,9 @@ const Hero = ({ scrollToSection, aboutRef}) => {
                 <h1 className="py-2 text-3xl font-semibold pl-1 font-alumni text-gray-300">Committed to Excellence</h1>
                 <h2 className="py-2 text-3xl font-semibold pl-1 font-alumni text-gray-300">Aspiring Software Engineer and second-year student at RMIT University in Melbourne, passionate about developing innovative solutions and continuously expanding my technical expertise.</h2>
                 <div className="flex gap-x-4 pr-16 py-2">
+                    <a href="/RhysCarrollResume2025.pdf" target="_blank" rel="noopener noreferrer">
                     <button className="text-neutral-900 font-alumni text-3xl bg-blue-500 rounded-md shadow-lg hover:shadow-blue-500 hover:bg-blue-500 transition duration-300 font-bold px-8 py-2">Resume</button>
+                    </a>
                     <button onClick={() => scrollToSection(aboutRef)} className="font-alumni border-blue-500 text-blue-500 border-2 rounded-md shadow-lg hover:shadow-blue-500 hover:text-blue-500 hover:border-blue-500 transition duration-300 text-3xl font-bold px-8 py-2">Learn More
                         <i className="fa fa-chevron-down text-xl pl-2 pb-1"></i>
                     </button>
